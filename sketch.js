df1fa666993b518948a5e30dc9055545aeb353d8
fidellla <103606027+fidellla@users.noreply.github.com>
2022-04-24 15:53:45 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+let posx = 100
+let posy = 100
+let csize = 10
+
+function setup() {
+  createCanvas(500,500);
+  background(0,150,175);
+}
+
+function draw() {
+background(0,150,175);
+noStroke(0)
+fill(0,200,150)
+circle(100,100,75)
+ fill(0,255,100)
+if (mouseX > 250){
+  rect(250,0,250,500)
+}
+
+  }
